a3039cd9014d122c28f9d498a327c6e2a09b9644
shunsukeumemura <umes0714@gmail.com>
2017-11-23 15:59:53 +0900

    LocationEntityの作成

---
+++ NavigationForiOS/LocatonEntity.swift
@@ -0,0 +1,19 @@
+//
+//  LocatonEntity.swift
+//  PrepareNavigationForiOS
+//
+//  Created by 梅村俊介 on 2017/11/23.
+//  Copyright © 2017年 UmeSystems. All rights reserved.
+//
+
+import Foundation
+
+class LocationEntity{
+    var name: String?
+    var detail: String?
+    
+    init(name: String, detail: String){
+        self.name = name
+        self.detail = detail
+    }
+}
