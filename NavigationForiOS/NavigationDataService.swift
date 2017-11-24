1f5fdbb66ee502c0c4f15a374b42efb97726882c
Minajun <s1620678@u.tsukuba.ac.jp>
2017-11-24 12:32:15 +0900

    hoge

--- NavigationForiOS/NavigationDataService.swift
+++ NavigationForiOS/NavigationDataService.swift
@@ -11,7 +11,7 @@ import Alamofire
 
 class NavigationDataService{
     //↓のURLでお願いします
-    private let apiUrl = "http://minajun.local/api/routes/0kzrV"
+    private let apiUrl = "http://localhost/api/routes/0kzrV"
     let sessionManager = Alamofire.SessionManager.default
     
     //ナビゲーションデータを送信する
